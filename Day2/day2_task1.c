ee5d3dfb1c8aed453091f5b3ebf35feee3437ea9
noarazemi10 <na279@bgt.school>
2026-04-15 09:49:59 +0200

    Update day2_task1.c

--- Day2/day2_task1.c
+++ Day2/day2_task1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int age = 16;
+    char initial = 'N';
+    float height = 1.78;
+    double average = 3.0;
+
+    short bonusPoints = 120;
+    long population = 1800000;
+
+    printf("\n===== PROFILI =====\n");
+    printf("Mosha: %d\n", age);
+    printf("Iniciali: %c\n", initial);
+    printf("Gjatesia: %.2f m\n", height);
+    printf("Mesatarja: %.2lf\n", average);
+    printf("Piket shtese (short): %d\n", bonusPoints);
+    printf("Popullsia (long): %ld\n", population);
+
+    return 0;
+}
